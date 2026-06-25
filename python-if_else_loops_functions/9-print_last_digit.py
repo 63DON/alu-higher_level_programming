799902e22baac8c9ed75c8d5e18c1c198e87c918
Don <b.don@alustudent.com>
2026-06-25 11:43:09 +0200

    Fix last digit calculation for negative numbers

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,8 @@
 
 def print_last_digit(number):
     """Print the last digit of a number and return its value."""
-    last_digit = abs(number) % 10
+    last_digit = number % 10
+    if number < 0 and last_digit != 0:
+        last_digit -= 10
     print(last_digit, end="")
     return last_digit
