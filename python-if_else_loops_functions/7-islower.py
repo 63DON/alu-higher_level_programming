c54096198140455d40e249ea07c67b31aba5cf91
Don <b.don@alustudent.com>
2026-06-25 03:18:18 +0200

    Implement islower function to check lowercase
    Add a check for lowercase characters using ASCII values.

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a function that checks for a lowercase character."""
 
+
 def islower(c):
     """Return True if c is lowercase, False otherwise."""
     if ord(c) >= 97 and ord(c) <= 122:
