b96de2c4cb2b17b15809415ebe75453081e6d0e7
Don <b.don@alustudent.com>
2026-06-26 17:17:29 +0200

    Add script to list non-hidden attributes in hidden_4
    Prints all non-hidden attributes from the hidden_4 module.

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Print all the names of attributes defined in hidden_4 module"""
+import hidden_4
+
+if __name__ == "__main__":
+    for name in sorted(dir(hidden_4)):
+        if not name.startswith("__"):
+            print(name)
