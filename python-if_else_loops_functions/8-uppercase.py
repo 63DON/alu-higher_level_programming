57f09f63762b5274bdd8539ae1f31495310273f6
Don <b.don@alustudent.com>
2026-06-26 16:32:08 +0200

    Fix uppercase to use single print statement
    Refactor uppercase function to build result string instead of printing directly.

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,9 +4,10 @@
 
 def uppercase(str):
     """Print a string in uppercase, followed by a new line."""
+    result = ""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            print("{:c}".format(ord(c) - 32), end="")
+            result += "{:c}".format(ord(c) - 32)
         else:
-            print("{:c}".format(ord(c)), end="")
-    print("")
+            result += "{:c}".format(ord(c))
+    print("{}".format(result))
