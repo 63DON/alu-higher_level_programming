890ebabbc9d206b2f6ff87f87f151c10b24efa1f
Don <b.don@alustudent.com>
2026-06-26 17:06:59 +0200

    Create 0-add.py to demonstrate addition
    Import the add function and print the result of an addition.

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Import the add function and print the result of an addition"""
+from add_0 import add
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
